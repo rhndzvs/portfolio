c267c9a1ed34e914b3928f788698dc2eb28b6204
Rhundei Zen Ballesteros <deiballesteros17@gmail.com>
2026-03-03 04:37:06 +0800

    add skills section with technology stack and interactive buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,21 @@ let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
 };
+
+const skillsBtns = document.querySelectorAll('.skills-btn');
+
+skillsBtns.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+        const skillDetails = document.querySelectorAll('.skills-detail');
+
+        skillsBtns.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        btn.classList.add('active');
+
+        skillDetails.forEach(detail => {
+            detail.classList.remove('active');
+        });
+        skillDetails[idx].classList.add('active');
+    });
+});
