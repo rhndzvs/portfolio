f3334305852a2647020996a620edd84143238c87
Rhundei Zen Ballesteros <deiballesteros17@gmail.com>
2026-03-03 16:40:08 +0800

    add scroll reveal functionality for enhanced animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,16 @@ darkModeIcon.onclick = () => {
     darkModeIcon.classList.toggle('bx-sun');
     document.body.classList.toggle('dark-mode');
 }
+
+/*========== scroll reveal ==========*/
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .skills-container, .projects-box, .contact-container', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
