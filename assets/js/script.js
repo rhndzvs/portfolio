fa72d6ffbde52e092b6000eb7a288bf4966ea6e2
Rhundei Zen Ballesteros <deiballesteros17@gmail.com>
2026-03-03 16:18:50 +0800

    add responsive menu icon and navbar functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+/*========== menu icon navbar ==========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 /*========== scroll sections active link ==========*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -21,6 +30,10 @@ window.onscroll = () => {
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
+
+/*========== remove menu icon navbar when click navbar link (scroll) ==========*/
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
 
 const skillsBtns = document.querySelectorAll('.skills-btn');
